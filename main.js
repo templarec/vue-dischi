785c4734302743f09345e0f85a64558543c77784
Lorenzo Bernini <templarec@gmail.com>
2021-04-09 18:23:46 +0200

    Esercizio completato + bonus 1 + bonus 2

--- main.js
+++ main.js
@@ -6,22 +6,26 @@
 var app = new Vue({
 	el: '#root',
 	data: {
-		album: [],
-		generi: [],
-		currentGenre: ''
+		album: [], //array per i dati da api
+		generi: [], //array per i generi nella select
+		currentGenre: '' //genere selezionato
 	},
 	mounted () {
 		axios.get('https://flynn.boolean.careers/exercises/api/array/music')
 			.then((risposta) => {
-				this.album = risposta.data.response;
+				this.album = risposta.data.response; //invia array oggetti da API al mio array
 				this.album.forEach((item)=>{
-					if (!this.generi.includes(item.genre)) {
+					if (!this.generi.includes(item.genre)) { //se non è incluso pusha il genere
 						this.generi.push(item.genre)
 					}
 
 				})
-
+				//ordinamento in base all'anno
+				this.album.sort((a,b) => {
+					return a.year - b.year;
+				})
 			});
+
 
 	},
 	computed: {},
